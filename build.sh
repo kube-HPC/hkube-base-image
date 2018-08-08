30794763a5845c6a2b8d9672aee2a7236ef1715e
Yehiyam Livneh <yehiyam@gmail.com>
2018-08-08 12:06:24 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-docker build -t hkube/base-node:v1.0.0 .
-docker push hkube/base-node:v1.0.0
+if [ -z $npm_package_version ]
+then
+  VERSION=latest
+else
+  VERSION="v${npm_package_version}"
+fi
+
+echo VERSION=$VERSION
+docker build -t hkube/base-node:${VERSION} .
+docker push hkube/base-node:$VERSION
